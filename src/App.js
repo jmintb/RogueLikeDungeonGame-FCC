74908eafe52dfaef6579a31a307cef9ce2060d06
Jessie Chatham Spencer <isai@protonmail.com>
2017-06-29 01:53:39 +0200

    React app files and some wip basic game structure.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,77 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const typeClasses = {
+  wall: 'wall',
+  ground: 'ground',
+  enemy: 'enemy',
+  health: 'health',
+  weapon: 'weapon',
+  boss: 'boss',
+  player: 'player'
+}
+
+const typeNumbers = {
+  wall: 0,
+  ground: 1,
+  enemy: 2,
+  health: 3,
+  weapon: 4,
+  boss: 5,
+  player: 6
+}
+
+class RogueLike extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return(
+      <div>
+        <Grid grid={this.state.grid}/>
       </div>
     );
   }
 }
 
-export default App;
+class Grid extends Component {
+  constructor(props) {
+    super(props);
+    this.createGrid = this.createGrid.bind(this);
+  }
+
+  createGrid() {
+    var currentGrid = this.props.grid;
+    var divRows = [];
+
+    for (var row = 0; row < currentGrid.length; row++) {
+      let divRow = [];
+      for (var column = 0; column < array.length; column++) {
+        divRow.push(<Tile type={currentGrid[row][column].type}/>)
+      }
+      divRows.push(<div className="row">{divRow}</div>);
+    }
+
+    return divRows;
+  }
+
+  render() {
+    return(
+      <div>
+        {this.createGrid()}
+      </div>
+    );
+  }
+}
+
+class Tile extends Component {
+  render(){
+    return(
+      <div className={"tile " + this.props.class}></div>
+    );
+  }
+}
+
+export default RogueLike;
