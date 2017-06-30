16ae514c85d3bb0ae05fbfebf1ea44db783f925c
Jessie Chatham Spencer <isai@protonmail.com>
2017-06-30 20:52:25 +0200

    Switched map to numbers, to minimize the size of the map. Also put a delay on the input handling to stop the game from having to update too quickly when the arrow keys are held down.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const gridHeight = 10;
-const gridWidth = 10;
+const gridHeight = 30;
+const gridWidth = 30;
+
+const visibleSquareHeight = 7;
+const visibleSquarewidth = 7;
 
 const typeClasses = {
   wall: 'wall',
@@ -15,13 +18,13 @@ const typeClasses = {
 }
 
 const typeNumbers = {
-  wall: 0,
-  ground: 1,
-  enemy: 2,
-  health: 3,
-  weapon: 4,
-  boss: 5,
-  player: 6
+  ground: 0,
+  wall: 1
+}
+
+const structureType = {
+  labyrinth: 0,
+  square: 1
 }
 
 const keys = {
@@ -32,19 +35,58 @@ const keys = {
 }
 
 const levelOneMap = [
-  [typeClasses.ground, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground],
-  [typeClasses.ground, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground],
-  [typeClasses.ground, typeClasses.ground, typeClasses.wall, typeClasses.wall, typeClasses.wall, typeClasses.wall, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground],
-  [typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground],
-  [typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground],
-  [typeClasses.wall, typeClasses.wall, typeClasses.wall, typeClasses.wall, typeClasses.wall, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground],
-  [typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground],
-  [typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground],
-  [typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground],
-  [typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.wall, typeClasses.ground, typeClasses.ground, typeClasses.ground]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
 ]
 
 const playerStartPosition = [0, 0];
+
+function getTypeClasses(typeNumber) {
+  var classes = ''; 
+  if(typeNumber === typeNumbers.ground) {
+    classes =  typeClasses.ground;
+  } else if(typeNumber === typeNumbers.wall) {
+    classes = typeClasses.wall;
+  }
+  return classes;
+}
+
+function convertMapToClasses(map) {
+  var convertedMap = map.slice();
+  for (var row = 0; row < map.length; row++) {
+    for (var column = 0; column < map[row].length; column++) {
+      convertedMap[row][column] = getTypeClasses(map[row][column]);
+    }    
+  }
+  return convertedMap;
+}
+
+function createRandomMap() {
+  var randomMap = [];
+  var playerStartPosition = [0, Math.trunc(Math.random()*30)-1]
+
+}
 
 class RogueLike extends Component {
   constructor(props) {
@@ -52,13 +94,18 @@ class RogueLike extends Component {
     this.handleInput = this.handleInput.bind(this);
     this.updatePlayerPosition = this.updatePlayerPosition.bind(this);
     document.onkeydown = this.handleInput;
-    this.state = {grid: levelOneMap, playerPosition: playerStartPosition};
+    this.state = {grid: convertMapToClasses(levelOneMap), playerPosition: playerStartPosition, inputBlocked: false};
+    createRandomMap();
   }
 
   handleInput(event) {
-    console.log('key: '+event.keyCode);
-    if(37 <= event.keyCode <= 40) {
+    if(37 <= event.keyCode <= 40 && !this.state.inputBlocked) {
       this.updatePlayerPosition(event.keyCode);
+      
+      this.setState({inputBlocked: true});
+      setTimeout(() => {
+        this.setState({inputBlocked: false});
+      }, 10);
     }
   }
 
@@ -67,13 +114,14 @@ class RogueLike extends Component {
 
     if(key === keys.up && playerPosition[0] > 0) {
       playerPosition[0] --;
-    } else if (key === keys.down && playerPosition[0] < gridHeight-1) {
+    } else if (key === keys.down && playerPosition[0] < this.state.grid.length-1) {
       playerPosition[0] ++;
     } else if (key === keys.left && playerPosition[1] > 0) {
       playerPosition[1] --;
-    } else if (key === keys.right && playerPosition[1] < gridWidth-1) {
+    } else if (key === keys.right && playerPosition[1] < this.state.grid[0].length-1) {
       playerPosition[1] ++;
     }
+
     if(this.state.grid[playerPosition[0]][playerPosition[1]] === typeClasses.ground){
       this.setState({playerPosition: playerPosition});
     }
@@ -105,7 +153,7 @@ class Grid extends Component {
       let divRow = [];
       for (var column = 0; column < currentGrid[row].length; column++) {
         let isPlayerPosition = row ===  this.props.playerPosition[0] && column === this.props.playerPosition[1];
-        let classes =  isPlayerPosition ? typeClasses.player : currentGrid[row][column];
+        let classes =  isPlayerPosition ? typeClasses.player : (currentGrid[row][column]);
         divRow.push(<Tile key={column+row} type={classes}/>)
       }
       divRows.push(<div key={row} className="row">{divRow}</div>);
